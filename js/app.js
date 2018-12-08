36c7dbb92e3d783e0d5b2429fbbaf0e996083ade
minghui001 <41600645+minghui001@users.noreply.github.com>
2018-12-09 00:47:31 +0800

    更新网站模板
    Co-Authored-By: minghui01 <minghui001@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation()
+
+/*
+nav-item
+当鼠标移动的此标签后，激活下拉列表、展开列表；
+当鼠标离开后，删除激活的下拉列表、停止
+*/
+$(function () {
+	$(".nav-item").on("mouseenter", function () {
+		$(this).addClass("show")
+		$(this).find(".nav_side").slideDown();
+	}).on("mouseleave", function () {
+		$(this).removeClass("show");
+		$(this).find(".nav_side").stop().slideUp(200);
+	})
+});
